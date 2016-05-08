44ed39ec68bec5bf8f70075d3567dc641ecca331
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-05-08 18:08:24 -0300

    removing unnecessary properties from github response when fetching code while logged out

--- src/voxel/coding/github.js
+++ src/voxel/coding/github.js
@@ -8,18 +8,7 @@ export default {
       method: 'GET'
     }).then(response => response.json()).then(response => {
       return {
-        id: response.id,
-        code: response.files[SINGLE_FILENAME].content,
-        author: response.owner ? {
-          id: response.owner.id,
-          avatar: response.owner.avatar_url,
-          login: response.owner.login
-        } : {
-          id: null,
-          avatar: 'https://avatars.githubusercontent.com/u/148100?v=3',
-          login: 'anonymous'
-        },
-        url: response.html_url
+        code: response.files[SINGLE_FILENAME].content
       };
     });
   }
