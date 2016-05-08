0217ac346b9f96abd242f3ed45fc9428cf4926aa
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-05-08 01:18:02 -0300

    passing coding controller to es6

--- src/voxel/coding/controller.js
+++ src/voxel/coding/controller.js
@@ -18,7 +18,7 @@ function resolveCode(blockType) {
   }
 }
 
-async function processNewBlockType(position, blockType) { // FIXME this still assumes block types come with code resolved...
+async function processNewBlockType(position, blockType) {
   types.add(blockType);
 
   let codeObj = await resolveCode(blockType);
@@ -54,17 +54,19 @@ export default {
     delete blocksWithCode[position];
     executor.remove(position);
   },
-  modifyPrototype(position, code) {
-    return fetch(consts.SERVER_ADDRESS() + '/inventory/blockType/' + blocksWithCode[position], {
+  async modifyPrototype(position, code) {
+    let blockType = await fetch(`${consts.SERVER_ADDRESS()}/inventory/blockType/${blocksWithCode[position]}`, {
       method: 'PUT',
       body: JSON.stringify({
         code
       }),
       headers: auth.getAuthHeaders()
-    }).then(response => response.json()).then(blockType => processNewBlockType(position, blockType));
+    }).then(response => response.json());
+
+    return processNewBlockType(blockType);
   },
-  forkPrototype(position, code, name) { // FIXME this shares lots of code with the method above!
-    return fetch(consts.SERVER_ADDRESS() + '/inventory/blockType/' + blocksWithCode[position] + '/fork', {
+  async forkPrototype(position, code, name) {
+    let blockType = await fetch(`${consts.SERVER_ADDRESS()}/inventory/blockType/${blocksWithCode[position]}/fork`, {
       method: 'POST',
       body: JSON.stringify({
         code,
@@ -72,10 +74,12 @@ export default {
         material: this.voxelEngine.getBlock(position)
       }),
       headers: auth.getAuthHeaders()
-    }).then(response => response.json()).then(blockType => processNewBlockType(position, blockType));
+    }).then(response => response.json());
+
+    return processNewBlockType(blockType);
   },
-  createNewPrototype(position, code, name) {
-    return fetch(consts.SERVER_ADDRESS() + '/inventory/blockType', {
+  async createNewPrototype(position, code, name) {
+    let blockType = await fetch(`${consts.SERVER_ADDRESS()}/inventory/blockType`, {
       method: 'POST',
       body: JSON.stringify({
         code,
@@ -83,6 +87,8 @@ export default {
         material: this.voxelEngine.getBlock(position)
       }),
       headers: auth.getAuthHeaders()
-    }).then(response => response.json()).then(blockType => processNewBlockType(position, blockType));
+    }).then(response => response.json());
+
+    return processNewBlockType(blockType);
   }
 };
