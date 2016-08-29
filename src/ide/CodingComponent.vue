cf0cc61f240654e4a319da0bc7e6cd3f58601d9e
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-08-28 23:08:18 -0300

    implementing permissions modification for chunks + using github login instead of id

--- src/ide/CodingComponent.vue
+++ src/ide/CodingComponent.vue
@@ -19,10 +19,10 @@
           <div v-if="item">
             <div class="author-info">
               <h2>Item/block owner</h2>
-              <div>{{item.owner.name}} <span v-if="mine">(a.k.a. you)</span></div>
+              <div>{{item.owner}} <span v-if="mine">(a.k.a. you)</span></div>
 
               <h2>Gist Author</h2>
-              <div>{{code.author.login}} <img class="author-avatar" :src="code.author.avatar"></div>
+              <div>{{code.author.id}} <img class="author-avatar" :src="code.author.avatar"></div>
             </div>
             <div class="gist-info">
               <h2>Gist info</h2>
@@ -76,7 +76,8 @@ export default {
       return this.code.revision.date != this.code.lastUpdateDate;
     },
     mine() {
-      return this.item.owner.id == auth.getUserId();
+      let currentUser = auth.getUserId();
+      return currentUser == this.item.owner && (!this.code || currentUser == this.code.author.id);
     }
   },
   methods: {
--- src/voxel/permissions.js
+++ src/voxel/permissions.js
@@ -1,8 +1,23 @@
 import client from './voxelClient';
+import requests from '../requests';
+import auth from '../auth';
 
 export default {
-  editChunkPermissions(pos) {
+  async editChunkPermissions(pos) {
     let chunk = client.getChunkAtPosition(pos);
-    console.log(chunk.position, chunk.owners)
+
+    let newOwnerId = prompt(`Existing owners of chunk ${chunk.id}: ${chunk.owners.join(',')} - add new one?`);
+
+    if(newOwnerId) {
+      await requests.requestToServer(`voxel/chunk/${chunk.id}/owners`, {
+        body: JSON.stringify({
+          newOwnerId
+        }),
+        method: 'POST',
+        headers: auth.getAuthHeaders()
+      });
+
+      alert(`Added ${newOwnerId} to list of owners`);
+    }
   }
 };
