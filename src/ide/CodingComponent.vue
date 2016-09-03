9db2f8c342d6e8bfe9449f3dbb4ae4a41272942b
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-09-03 03:52:44 -0300

    fix in ide for new block types

--- src/ide/CodingComponent.vue
+++ src/ide/CodingComponent.vue
@@ -12,7 +12,7 @@
         <img v-if="item" class="item-icon" :src="'assets/img/icons/' + item.icon + '.png'">
 
         <span v-if="testingLocally">| This code is being tested locally</span>
-        <span v-if="simpleBlock && !testingLocally">| This block type had no code</span>
+        <span v-if="item && simpleBlock && !testingLocally">| This block type had no code</span>
       </h1>
 
       <div v-el:close class="closeButton" @click="close"></div>
